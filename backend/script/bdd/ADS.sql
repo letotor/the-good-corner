0e92dfacb9cae5becd9010a3884cc71d2b7d5bd9
victorDGW <victor-de-germain_student2022@wilder.school>
2023-09-25 22:58:21 +0200

    generate BDD

--- backend/script/bdd/ADS.sql
+++ backend/script/bdd/ADS.sql
@@ -1,6 +1,3 @@
--- SQLite
-
-
 -- CREATION DE LA TABLE CATEGORY 
 CREATE TABLE CATEGORY (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,8 +12,9 @@ CREATE TABLE ADS (
     price INTEGER,
     picture varchar(128),
     location varchar(50),
+    owner varchar(50),
     category  INTEGER,
-    date DATE,
+    dateAtCreated DATE,
     FOREIGN KEY(category) REFERENCES CATEGORY(id)
 );
 
@@ -24,40 +22,36 @@ CREATE TABLE ADS (
 
 -- INSERTION DES DONNEE DANS LA TABLE CATEGORY
 PRAGMA foreign_keys = ON;
-INSERT INTO CATEGORY (name) VALUES ('Vêtement'),,('Téléphonie'), ('Immobilier'), ('Véhicules'), ('Informatique'), ('Maison'), ('Emploi'), ('Loisirs'), ('Mode'), ('Multimédia'), ('Hifi'), ('Matériel professionnel'), ('Musique'), ('Montres & Bijoux'), ('Jeux vidéo & Consoles'), ('Sacs & Accessoires'), ('Autres');
+INSERT INTO CATEGORY (name) VALUES ('Vêtement'),('Téléphonie'), ('Immobilier'), ('Véhicules'), ('Informatique'), ('Maison'), ('Emploi'), ('Loisirs'), ('Mode'), ('Multimédia'), ('Hifi'), ('Matériel professionnel'), ('Musique'), ('Montres & Bijoux'), ('Jeux vidéo & Consoles'), ('Sacs & Accessoires'), ('Autres');
 
-
--- TEST insertion ORPHAN
-PRAGMA foreign_keys = ON;
-INSERT INTO ADS (title, description, price, picture, location, category, date) VALUES ('iPhone 13 Pro', 'iPhone 13 Pro 256 Go en excellent état', 900, 'iphone.jpg', 'Paris', 30, '2023-09-20');
 -- VIDER LA TABLE si NECESSAIRE
 --  TRUNCATE TABLE ad;
+
+
 -- INSERTION DES DONNEe
 PRAGMA foreign_keys = ON;
-INSERT INTO ADS (title, description, price, picture, location, category, date)
+INSERT INTO Ads (title, description, price, picture, location, category, dateAtCreated,owner )
 VALUES
-  ('iPhone 13 Pro', 'iPhone 13 Pro 256 Go en excellent état', 900, 'iphone.jpg', 'Paris', 1, '2023-09-20'),
-  ('iPhone 12 Pro', 'iPhone 12 bon état', 300, 'iphone.jpg', 'Paris', 1, '2022-09-20'),
-    ('Appartement à louer', 'Appartement 2 pièces, proche du centre-ville', 800, 'appartement.jpg', 'Lyon', 2, '2023-09-21'),
-    ('Vélo de montagne', 'Vélo de montagne tout suspendu, en parfait état', 300, 'velo.jpg', 'Bordeaux', 3, '2023-09-22'),
-    ('Vélo de ville', 'Vélo de ville suspendu, en  état', 200, 'velo.jpg', 'Bordeaux', 3, '2023-09-22'),
-    ('Ordinateur portable Dell', 'Ordinateur portable Dell XPS 15, 16 Go de RAM', 1200, 'dell.jpg', 'Paris', 4, '2023-09-23'),
-    ('Studio meublé', 'Studio meublé, toutes charges comprises', 600, 'studio.jpg', 'Lyon', 2, '2023-09-24'),
-    ('Table de salle à manger', 'Table en bois massif avec 6 chaises assorties', 250, 'table.jpg', 'Bordeaux', 5, '2023-09-25'),
-    ('Télévision Samsung 55 pouces', 'Télévision 4K, Smart TV avec télécommande', 600, 'tv.jpg', 'Paris', 5, '2023-09-26'),
-    ('Voiture d occasion', 'Toyota Camry 2019, en excellent état', 15000, 'voiture.jpg', 'Lyon', 6, '2023-09-27'),
-    ('Cuisine équipée', 'Cuisine moderne avec électroménagers inclus', 2500, 'cuisine.jpg', 'Bordeaux', 7, '2023-09-28'),
-    ('Canapé en cuir', 'Canapé en cuir véritable, 3 places', 800, 'canape.jpg', 'Paris', 8, '2023-09-29'),
-    ('MacBook Pro 13 pouces', 'MacBook Pro avec processeur M1, 512 Go de stockage', 1400, 'macbook.jpg', 'Lyon', 4, '2023-09-30'),
-    ('Appartement de luxe', 'Appartement de 4 pièces avec vue sur la rivière', 2500, 'appartement_luxe.jpg', 'Bordeaux', 2, '2023-10-01'),
-    ('Vélo de route', 'Vélo de route en carbone avec groupe Shimano 105', 1200, 'velo_route.jpg', 'Paris', 3, '2023-10-02'),
-    ('Tablet Samsung Galaxy', 'Tablet Samsung Galaxy Tab S7, écran AMOLED', 400, 'tablet.jpg', 'Lyon', 9, '2023-10-03'),
-    ('Piano à queue Steinway', 'Piano à queue Steinway & Sons, son exceptionnel', 15000, 'piano.jpg', 'Bordeaux', 10, '2023-10-04'),
-    ('Chaise de bureau ergonomique', 'Chaise de bureau avec support lombaire réglable', 200, 'chaise_bureau.jpg', 'Paris', 11, '2023-10-05'),
-    ('Guitare acoustique', 'Guitare acoustique de qualité professionnelle', 800, 'guitare.jpg', 'Bordeaux', 12, '2023-10-06'),
-    ('Montre Rolex Submariner', 'Montre de plongée Rolex Submariner en acier', 10000, 'rolex.jpg', 'Paris', 13, '2023-10-07'),
-    ('Ordinateur de jeu', 'PC de jeu haut de gamme avec carte graphique RTX', 1800, 'ordinateur_gaming.jpg', 'Lyon', 14, '2023-10-08'),
-    ('Sac à main de créateur', 'Sac à main de créateur en cuir véritable', 500, 'sac.jpg', 'Bordeaux', 15, '2023-10-09');
+  ('iPhone 13 Pro', 'iPhone 13 Pro 256 Go en excellent état', 900, 'iphone.jpg', 'Paris', 1, '2023-09-20', 'Jean Dupont'),
+  ('iPhone 12 Pro', 'iPhone 12 bon état', 300, 'iphone.jpg', 'Paris', 1, '2022-09-20', 'Marie Martin'),
+  ('Appartement à louer', 'Appartement 2 pièces, proche du centre-ville', 800, 'appartement.jpg', 'Lyon', 2, '2023-09-21', 'Pierre Tremblay'),
+  ('Vélo de montagne', 'Vélo de montagne tout suspendu, en parfait état', 300, 'velo.jpg', 'Bordeaux', 3, '2023-09-22', 'Sophie Lefebvre'),
+  ('Vélo de ville', 'Vélo de ville suspendu, en  état', 200, 'velo.jpg', 'Bordeaux', 3, '2023-09-22', 'Pauline Dubois'),
+  ('Ordinateur portable Dell', 'Ordinateur portable Dell XPS 15, 16 Go de RAM', 1200, 'dell.jpg', 'Paris', 4, '2023-09-23', 'Nicolas Leclerc'),
+  ('Studio meublé', 'Studio meublé, toutes charges comprises', 600, 'studio.jpg', 'Lyon', 2, '2023-09-24', 'Lucie Girard'),
+  ('Table de salle à manger', 'Table en bois massif avec 6 chaises assorties', 250, 'table.jpg', 'Bordeaux', 5, '2023-09-25', 'Antoine Gagnon'),
+  ('Télévision Samsung 55 pouces', 'Télévision 4K, Smart TV avec télécommande', 600, 'tv.jpg', 'Paris', 5, '2023-09-26', 'Émilie Caron'),
+  ('Voiture d occasion', 'Toyota Camry 2019, en excellent état', 15000, 'voiture.jpg', 'Lyon', 6, '2023-09-27', 'Thomas Bergeron'),
+  ('Cuisine équipée', 'Cuisine moderne avec électroménagers inclus', 2500, 'cuisine.jpg', 'Bordeaux', 7, '2023-09-28', 'Marc Dupuis'),
+  ('Canapé en cuir', 'Canapé en cuir véritable, 3 places', 800, 'canape.jpg', 'Paris', 8, '2023-09-29', 'Julie Lambert'),
+  ('MacBook Pro 13 pouces', 'MacBook Pro avec processeur M1, 512 Go de stockage', 1400, 'macbook.jpg', 'Lyon', 4, '2023-09-30', 'Philippe Lavoie'),
+  ('Appartement de luxe', 'Appartement de 4 pièces avec vue sur la rivière', 2500, 'appartement_luxe.jpg', 'Bordeaux', 2, '2023-10-01', 'Isabelle Morin'),
+  ('Vélo de route', 'Vélo de route en carbone avec groupe Shimano 105', 1200, 'velo_route.jpg', 'Paris', 3, '2023-10-02', 'Sébastien Roy'),
+  ('Tablet Samsung Galaxy', 'Tablet Samsung Galaxy Tab S7, écran AMOLED', 400, 'tablet.jpg', 'Lyon', 9, '2023-10-03', 'Valérie Boucher'),
+  ('Piano à queue Steinway', 'Piano à queue Steinway & Sons, son exceptionnel', 15000, 'piano.jpg', 'Bordeaux', 10, '2023-10-04', 'François Gagné'),
+  ('Chaise de bureau ergonomique', 'Chaise de bureau avec support lombaire réglable', 200, 'chaise_bureau.jpg', 'Paris', 11, '2023-10-05', 'Nathalie Lévesque'),
+  ('Guitare acoustique', 'Guitare acoustique de qualité professionnelle', 800, 'guitare.jpg', 'Bordeaux', 12, '2023-10-06', 'Michel Tremblay'),
+  ('Montre Rolex Submariner', 'Montre de plongée Rolex Submariner en acier', 10000, 'rolex.jpg', 'Paris', 13, '2023-10-07', 'Caroline Gauthier');
 
 -- AFFICHAGE DES ANNONCES
 SELECT * FROM ADS;
